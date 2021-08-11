6273011067981577458e8ab2e12da371cd38063d
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-08-12 00:05:32 +0600

    24

--- script.js
+++ script.js
@@ -282,3 +282,13 @@
 // console.log(task25("Elsa"))
 // console.log(task25("Anastasia"))
 // console.log(task25("Egor"))
+
+
+// function task26(str) {
+//     return str.lastIndexOf("a")
+// }
+//
+// console.log(task26("Aurora"))
+// console.log(task26("Elsa"))
+// console.log(task26("Anastasia"))
+// console.log(task26("Egor"))
