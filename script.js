5bc6e4a37439147a3ed794336d6962ec264851de
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-08-11 23:55:26 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// function num2(a) {
+//     if (a > 0) {
+//         return "positive"
+//     } else if (a < 0) {
+//         return "negative"
+//     }
+//     return "equal"
+// }
+//
+// console.log(num2(2))
+// console.log(num2(-2))
+// console.log(num2(0))
